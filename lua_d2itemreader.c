7d1a5054f5df1dfa275cfcbdde1393e366911cc0
Ryan Liptak <squeek502@hotmail.com>
2018-09-01 13:30:49 -0700

    Bunch of stuff

--- lua_d2itemreader.c
+++ lua_d2itemreader.c
@@ -22,8 +22,8 @@
 #define set_string(L, k, v) set_field(string, L, k, v)
 #define set_lstring(L, k, v, l) set_lfield(lstring, L, k, v, l)
 #define set_integer(L, k, v) set_field(integer, L, k, v)
-#define set_boolean(L, k, v) set_field(boolean, L, k ,v)
-#define set_value(L, k, v) set_field(value, L, k ,v)
+#define set_boolean(L, k, v) set_field(boolean, L, k, v)
+#define set_value(L, k, v) set_field(value, L, k, v)
 
 static int push_error(lua_State *L, const char* msg)
 {
@@ -40,6 +40,21 @@ static int push_ferror(lua_State *L, const char *fmt, ...)
     lua_pushvfstring(L, fmt, args);
     va_end(args);
     return 2;
+}
+
+static int push_d2err(lua_State *L, d2err err)
+{
+	return push_error(L, d2err_str(err));
+}
+
+static void ld2itemreader_checkfield(lua_State *L, int idx, const char* k, int type)
+{
+	lua_getfield(L, idx, k);
+	if (lua_type(L, -1) != type)
+	{
+		const char *msg = lua_pushfstring(L, "expected the value of key '%s' to be of type %s, got %s", k, lua_typename(L, type), luaL_typename(L, idx));
+		luaL_argerror(L, idx, msg);
+	}
 }
 
 static void push_d2item_rarity_affixes(lua_State *L, uint16_t* affixes, uint8_t numAffixes)
@@ -110,6 +125,7 @@ static void push_d2item(lua_State *L, d2item* item)
 	lua_newtable(L);
 	set_boolean(L, "identified", item->identified);
 	set_string(L, "code", item->code);
+	set_boolean(L, "ethereal", item->ethereal);
 	if (!item->simpleItem)
 	{
 		if (item->id)
@@ -121,15 +137,18 @@ static void push_d2item(lua_State *L, d2item* item)
 		set_integer(L, "level", item->level);
 		push_d2item_set_rarity(L, item);
 	}
+	set_boolean(L, "runeword", item->isRuneword);
 	return;
 }
 
-static int ld2itemreader_get_items(lua_State *L)
+static int ld2itemreader_getitems(lua_State *L)
 {
+	d2err err;
 	static d2char character;
 	static d2personalstash pstash;
 	static d2sharedstash sstash;
-	static uint32_t bytesRead;
+	static d2atmastash astash;
+	static size_t bytesRead;
 	static int key;
 
 	const char* filepath = luaL_checkstring(L, 1);
@@ -141,19 +160,39 @@ static int ld2itemreader_get_items(lua_State *L)
 	switch(type)
 	{
 	case D2FILETYPE_D2_CHARACTER:
-		d2char_parse(filepath, &character, &bytesRead);
+		err = d2char_parse_file(filepath, &character, &bytesRead);
+		if (err != D2ERR_OK)
+		{
+			goto err;
+		}
 		for (int i=0; i<character.items.count; i++)
 		{
 			d2item* item = &character.items.items[i];
 			push_d2item(L, item);
-			lua_rawseti(L, -2, i+1);
+			lua_rawseti(L, -2, i + 1);
+		}
+		for (int i = 0; i<character.itemsCorpse.count; i++)
+		{
+			d2item* item = &character.itemsCorpse.items[i];
+			push_d2item(L, item);
+			lua_rawseti(L, -2, i + 1);
+		}
+		for (int i = 0; i<character.itemsMerc.count; i++)
+		{
+			d2item* item = &character.itemsMerc.items[i];
+			push_d2item(L, item);
+			lua_rawseti(L, -2, i + 1);
 		}
 		d2char_destroy(&character);
 		break;
 	case D2FILETYPE_PLUGY_PERSONAL_STASH:
-		d2personalstash_parse(filepath, &pstash, &bytesRead);
+		err = d2personalstash_parse_file(filepath, &pstash, &bytesRead);
+		if (err != D2ERR_OK)
+		{
+			goto err;
+		}
 		key=1;
-		for (int i=0; i<pstash.numPages; i++)
+		for (uint32_t i=0; i<pstash.numPages; i++)
 		{
 			d2stashpage* page = &pstash.pages[i];
 			for (int j=0; j<page->items.count; j++)
@@ -167,9 +206,13 @@ static int ld2itemreader_get_items(lua_State *L)
 		d2personalstash_destroy(&pstash);
 		break;
 	case D2FILETYPE_PLUGY_SHARED_STASH:
-		d2sharedstash_parse(filepath, &sstash, &bytesRead);
+		err = d2sharedstash_parse_file(filepath, &sstash, &bytesRead);
+		if (err != D2ERR_OK)
+		{
+			goto err;
+		}
 		key=1;
-		for (int i=0; i<sstash.numPages; i++)
+		for (uint32_t i=0; i<sstash.numPages; i++)
 		{
 			d2stashpage* page = &sstash.pages[i];
 			for (int j=0; j<page->items.count; j++)
@@ -182,79 +225,125 @@ static int ld2itemreader_get_items(lua_State *L)
 		}
 		d2sharedstash_destroy(&sstash);
 		break;
+	case D2FILETYPE_ATMA_STASH:
+		err = d2atmastash_parse_file(filepath, &astash, &bytesRead);
+		if (err != D2ERR_OK)
+		{
+			goto err;
+		}
+		for (int i = 0; i<astash.items.count; i++)
+		{
+			d2item* item = &astash.items.items[i];
+			push_d2item(L, item);
+			lua_rawseti(L, -2, i + 1);
+		}
+		d2atmastash_destroy(&astash);
+		break;
 	default:
 		return push_ferror(L, "unhandled filetype of %s: %d", filepath, type);
 		break;
 	}
 	return 1;
+
+err:
+	return push_ferror(L, "failed to parse %s: %s at byte 0x%X", filepath, d2err_str(err), bytesRead);
 }
 
-static int ld2itemreader_load_data(lua_State *L)
+static int ld2itemreader_loadfiles(lua_State *L)
 {
 	luaL_checktype(L, 1, LUA_TTABLE);
-	const char* filepath;
 
-	lua_getfield(L, 1, "armors");
-	filepath = luaL_checkstring(L, -1);
-	d2data_load_armors_from_file(filepath, &g_d2data);
+	d2datafiles files;
+
+	ld2itemreader_checkfield(L, 1, "armors", LUA_TSTRING);
+	files.armorTxtFilepath = lua_tostring(L, -1);
 	lua_pop(L, 1);
-	
-	lua_getfield(L, 1, "weapons");
-	filepath = luaL_checkstring(L, -1);
-	d2data_load_weapons_from_file(filepath, &g_d2data);
+
+	ld2itemreader_checkfield(L, 1, "weapons", LUA_TSTRING);
+	files.weaponsTxtFilepath = luaL_checkstring(L, -1);
 	lua_pop(L, 1);
-	
-	lua_getfield(L, 1, "miscs");
-	filepath = luaL_checkstring(L, -1);
-	d2data_load_miscs_from_file(filepath, &g_d2data);
+
+	ld2itemreader_checkfield(L, 1, "miscs", LUA_TSTRING);
+	files.miscTxtFilepath = luaL_checkstring(L, -1);
 	lua_pop(L, 1);
-	
-	lua_getfield(L, 1, "itemstats");
-	filepath = luaL_checkstring(L, -1);
-	d2data_load_itemstats_from_file(filepath, &g_d2data);
+
+	ld2itemreader_checkfield(L, 1, "itemstats", LUA_TSTRING);
+	files.itemStatCostTxtFilepath = luaL_checkstring(L, -1);
 	lua_pop(L, 1);
+
+	d2err err = d2itemreader_init_files(files);
+	if (err != D2ERR_OK)
+	{
+		return push_d2err(L, err);
+	}
 
 	return 0;
 }
 
-static int ld2itemreader_setup_data(lua_State *L)
+static int ld2itemreader_loaddata(lua_State *L)
 {
 	luaL_checktype(L, 1, LUA_TTABLE);
-	size_t len;
-	const char* data;
 
-	lua_getfield(L, 1, "armors");
-	data = luaL_checklstring(L, -1, &len);
-	d2data_load_armors(data, len, &g_d2data);
+	d2databufs bufs;
+
+	ld2itemreader_checkfield(L, 1, "armors", LUA_TSTRING);
+	bufs.armorTxt = (char*)lua_tolstring(L, -1, &bufs.armorTxtSize);
 	lua_pop(L, 1);
-	
-	lua_getfield(L, 1, "weapons");
-	data = luaL_checklstring(L, -1, &len);
-	d2data_load_weapons(data, len, &g_d2data);
+
+	ld2itemreader_checkfield(L, 1, "weapons", LUA_TSTRING);
+	bufs.weaponsTxt = (char*)lua_tolstring(L, -1, &bufs.weaponsTxtSize);
 	lua_pop(L, 1);
-	
-	lua_getfield(L, 1, "miscs");
-	data = luaL_checklstring(L, -1, &len);
-	d2data_load_miscs(data, len, &g_d2data);
+
+	ld2itemreader_checkfield(L, 1, "miscs", LUA_TSTRING);
+	bufs.miscTxt = (char*)lua_tolstring(L, -1, &bufs.miscTxtSize);
 	lua_pop(L, 1);
-	
-	lua_getfield(L, 1, "itemstats");
-	data = luaL_checklstring(L, -1, &len);
-	d2data_load_itemstats(data, len, &g_d2data);
+
+	ld2itemreader_checkfield(L, 1, "itemstats", LUA_TSTRING);
+	bufs.itemStatCostTxt = (char*)lua_tolstring(L, -1, &bufs.itemStatCostTxtSize);
 	lua_pop(L, 1);
+
+	d2err err = d2itemreader_init_bufs(bufs);
+	if (err != D2ERR_OK)
+	{
+		return push_d2err(L, err);
+	}
 
 	return 0;
 }
 
 static const luaL_Reg d2itemreader_lualib[] = {
-	{ "load_data", ld2itemreader_load_data },
-	{ "setup_data", ld2itemreader_setup_data },
-	{ "get_items", ld2itemreader_get_items },
+	{ "loadfiles", ld2itemreader_loadfiles },
+	{ "loaddata", ld2itemreader_loaddata },
+	{ "getitems", ld2itemreader_getitems },
 	{ NULL, NULL }
 };
 
+static int ld2itemreader_close(lua_State *L)
+{
+	d2itemreader_destroy();
+	return 0;
+}
+
+static const luaL_Reg d2itemreader_gc[] = {
+	{ "__gc", ld2itemreader_close },
+	{ NULL, NULL }
+};
+
+
 EXPORT int luaopen_d2itemreader(lua_State *L)
 {
+	d2err err = d2itemreader_init_default();
+	if (err != D2ERR_OK)
+	{
+		luaL_error(L, "failed to initialize d2itemreader: %s", d2err_str(err));
+	}
 	luaL_newlib(L, d2itemreader_lualib);
+
+	lua_newuserdata(L, 0);
+	lua_newtable(L);
+	luaL_setfuncs(L, d2itemreader_gc, 0);
+	lua_setmetatable(L, -2);
+	lua_setfield(L, -2, "__unload");
+
 	return 1;
 }
